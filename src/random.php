46a1fbe89fc195a226e946204372c31fe9d4fa83
Samy Kacem <samy@Samys-MacBook-Pro.local>
2018-12-03 07:47:30 +0100

    Add random page content

---
+++ src/random.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Demo Website - Login</title>
+    </head>
+    <body>
+            <?php
+            if (rand(0, 1) == 0) {
+                echo "Have a good day!<br>";
+            }
+            else
+            {
+                echo "Have a good week!<br>";
+            }
+            ?>
+    </body>
+</html>
